7fddb0992789fee3c123d2bdd24aa0bd014ba60f
lrobins <lrobins@adobe.com>
2015-02-10 19:25:52 -0700

    adds  promises and finished project

--- controller.js
+++ controller.js
@@ -1,9 +1,9 @@
-var app = angular.module('userProfiles');
+var app = angular.module('userProfiles'); 
 
 app.controller('MainController', function($scope, mainService) {
   $scope.getUsers = function() {
     $scope.users= mainService.getUsers().then(function(data) {
-      $scope.users = data.data.data;
+      $scope.users = data;
     });
   }
 
